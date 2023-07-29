c59bd54bb86484e1188cd760926a2e092746e50d
Mariocoro <135136166+Mariocoro@users.noreply.github.com>
2023-07-29 22:54:40 +0000

    question-3

--- src/shooter.cpp
+++ src/shooter.cpp
@@ -1,1 +1,28 @@
-//
+#include "shooter.h"
+#include <iostream>
+
+Shooter::Shooter() {
+
+}
+
+Shooter::~Shooter() {
+	for (Roll* r : rolls) {
+		delete r;
+	}
+}
+
+Roll* Shooter::throw_dice() {
+	Die d1, d2;
+	Roll* r = new Roll(&d1, &d2);
+	r->roll_die();
+	rolls.push_back(r);
+	return r;
+}
+
+std::ostream& operator<<(std::ostream& os, const Shooter& obj){
+	// os << obj.rolls;
+	for (Roll* r : obj.rolls){
+		os << r->roll_value() << "\n";
+	}
+	return os;
+}
--- src/shooter.h
+++ src/shooter.h
@@ -1,1 +1,13 @@
-//
+#pragma once
+#include "Roll.h"
+#include <vector>
+#include <ostream>
+class Shooter {
+private:
+	std::vector<Roll*> rolls;
+public:
+	Shooter();
+	~Shooter();
+	Roll* throw_dice();
+	friend std::ostream& operator<<(std::ostream& os, const Shooter& obj);
+};
