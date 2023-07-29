b87facbdfee2f6b81fb7c775cd0c017a9d08abaf
Mariocoro <135136166+Mariocoro@users.noreply.github.com>
2023-07-29 22:52:17 +0000

    question-2

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,1 +1,17 @@
-//
+#include "roll.h"
+Roll::Roll(Die* die1, Die* die2) {
+	this->die1 = die1;
+	this->die2 = die2;
+	rolled = false;
+}
+
+void Roll::roll_die() {
+	die1->roll();
+	die2->roll();
+	value = die1->roll_value() + die2->roll_value();
+	rolled = true;
+}
+
+int Roll::roll_value() const {
+	return value;
+}
--- src/roll.h
+++ src/roll.h
@@ -1,1 +1,13 @@
-//
+#pragma once
+#include "die.h"
+class Roll {
+private:
+	Die* die1;
+	Die* die2;
+	bool rolled;
+	int value;
+public:
+	Roll(Die* die1, Die* die2);
+	void roll_die();
+	int roll_value() const;
+};
