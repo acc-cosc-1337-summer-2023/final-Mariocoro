8a418ff01d34982b7d3b9423968f3a6049aa1eec
Mariocoro <135136166+Mariocoro@users.noreply.github.com>
2023-07-29 22:46:24 +0000

    question-1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,12 @@
-//
+#include "die.h"
+#include <cstdlib>
+Die::Die() {
+}
+
+void Die::roll() {
+	rolled_value = sides[rand() % 6];
+}
+
+int Die::roll_value() const {
+	return rolled_value;
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,10 @@
-//
+#pragma once
+class Die {
+private:
+	int rolled_value = 1;
+	int sides[6] = {1,2,3,4,5,6};
+public:
+	Die();
+	void roll();
+	int roll_value() const;
+};
